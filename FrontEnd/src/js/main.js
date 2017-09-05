e1d6901a3d29eb442c4dc95c35d6d32dfc1b39b0
finch <422465778@qq.com>
2017-09-05 14:26:10 +0800

    web app v1

---
+++ FrontEnd/src/js/main.js
@@ -0,0 +1,121 @@
+var base_url = "http://139.199.172.96:5000";
+//var type_byte = "byte";
+
+//var request_byte = `${base_url}${type_byte}`;
+var request_sol = base_url + '/sol';
+var request_bytecode = base_url + '/byte';
+
+var submit_type = 0;
+
+var sol_type = 0;
+var byte_type = 1;
+
+(function () {
+  $("#analysis_start").click(function () {
+    window.cm.save();
+    window.bytearea.save();
+    
+    
+    var sol_value = window.cm.getTextArea().value;
+    var byte_value = window.bytearea.getTextArea().value;
+    
+    // if (!sol_value) {
+    //   $(".alert").show();
+    //   return;
+    // }
+    //
+    // if (!byte_value) {
+    //   $(".alert").show();
+    //   return;
+    // }
+    //
+    // $(".alert").hide();
+    
+    $("#loading_main").show();
+    if (submit_type === sol_type) {
+      $.post(request_sol, {code: sol_value}, function (data, textStatus) {
+        console.log("data", data);
+        console.log("textStatus", textStatus);
+        $("#loading_main").hide();
+        if (!data.result.callstack) {
+          $("#pattern_result_arrows").addClass("safe");
+        } else {
+          $("#pattern_result_arrows").addClass("matched");
+        }
+        
+        if (!data.result.concurrency) {
+          $("#pattern_result_bug").addClass("safe");
+          
+        } else {
+          $("#pattern_result_bug").addClass("matched");
+        }
+        
+        if (!data.result.reentrancy) {
+          $("#pattern_result_filter").addClass("safe");
+        } else {
+          $("#pattern_result_filter").addClass("matched");
+        }
+        
+        if (!data.result.time_dependency) {
+          $("#pattern_result_refresh").addClass("safe");
+        } else {
+          $("#pattern_result_refresh").addClass("matched");
+        }
+        
+        $("#content_analysis").fadeIn();
+      });
+    } else {
+      $.post(request_bytecode, {code: byte_value}, function (data, textStatus) {
+        console.log("data", data);
+        console.log("textStatus", textStatus);
+        $("#loading_main").hide();
+        if (!data.result.callstack) {
+          $("#pattern_result_arrows").addClass("safe");
+        } else {
+          $("#pattern_result_arrows").addClass("matched");
+        }
+        
+        if (!data.result.concurrency) {
+          $("#pattern_result_bug").addClass("safe");
+          
+        } else {
+          $("#pattern_result_bug").addClass("matched");
+        }
+        
+        if (!data.result.reentrancy) {
+          $("#pattern_result_filter").addClass("safe");
+        } else {
+          $("#pattern_result_filter").addClass("matched");
+        }
+        
+        if (!data.result.time_dependency) {
+          $("#pattern_result_refresh").addClass("safe");
+        } else {
+          $("#pattern_result_refresh").addClass("matched");
+        }
+        
+        $("#content_analysis").fadeIn();
+      }).error(function () {
+        $("#loading_main").hide();
+      });
+    }
+  });
+  
+  $("#btn-solidity").click(function () {
+    submit_type = sol_type;
+    $("#btn-solidity").addClass("active");
+    $("#btn-bytecode").removeClass("active");
+    $("#code-solidity-wrapper").show();
+    $("#code-bytecode-wrapper").hide();
+  });
+  
+  $("#btn-bytecode").click(function () {
+    submit_type = byte_type;
+    $("#btn-bytecode").addClass("active");
+    $("#btn-solidity").removeClass("active");
+    $("#code-solidity-wrapper").hide();
+    $("#code-bytecode-wrapper").show();
+  });
+})();
+
+
